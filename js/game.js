e2c5572d0a7dfeb60f8a0c753b57ec7893349bca
wormps <wormpsps@gmail.com>
2025-01-24 15:03:01 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -6518,7 +6518,7 @@ $('#default-cursor-btn').click(function () {
         id = name;
         
         $.ajax({
-            url: 'https://wormps.github.io/extension/api/lan-da-vh.php',
+            url: 'https://wormps.github.io/extension/api/skins.php',
             method: 'GET',
             dataType: 'json',
             success: function (id) {
